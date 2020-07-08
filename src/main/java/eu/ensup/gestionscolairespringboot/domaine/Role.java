7e396bf1ed89813d483ce2c2c7be0fabad8221aa
DavidTOUCHARD <david.touchard@hotmail.fr>
2020-07-08 12:16:02 +0200

    Modif dans Role, generationType identity to AUTO

--- src/main/java/eu/ensup/gestionscolairespringboot/domaine/Role.java
+++ src/main/java/eu/ensup/gestionscolairespringboot/domaine/Role.java
@@ -11,7 +11,7 @@ import javax.persistence.Table;
 public class Role {
 	@Id
 	@Column(name = "role_id")
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
 	
 	private String name;
